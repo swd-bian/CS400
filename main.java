10e81c2e369f27d69c45a6f026bd9bed89c53c49
CMY <xxx.xxx.xxx>
2023-10-05 15:23:12 +0000

    it add a print upper

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main{
     public static void main(String[] args){
+	System.out.println("it is upper and is this depend on number or else?");
 	System.out.print("hello this is first line and the seperate line to determine is rely on up down or line number");
     }
 }
